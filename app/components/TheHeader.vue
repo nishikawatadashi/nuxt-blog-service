63363af4a4ea5eef5c71c50bfb24d58794a1d748
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2018-10-20 15:37:14 +0900

    共通レイアウトの作成

---
+++ app/components/TheHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-menu
+    :router="true"
+    mode="horizontal">
+    <el-menu-item
+      index="1"
+      style="pointer-events:none;">
+      Nuxt Diary App
+    </el-menu-item>
+    <el-menu-item
+      :router="{ path: '/posts/' }"
+      index="2">
+      投稿一覧
+    </el-menu-item>
+
+    <no-ssr>
+      <el-menu-item
+        :router="{ path: '/' }"
+        index="4"
+        style="float: right;">
+        <span>ログイン</span>
+      </el-menu-item>
+    </no-ssr>
+    <el-menu-item
+      :router="{ path: '/posts/new' }"
+      index="5"
+      style="float: right;">
+      新規投稿
+    </el-menu-item>
+  </el-menu>
+</template>
