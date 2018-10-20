b72cc08da05c88e4f4780edf1a8bc69a05ec77b4
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2018-10-21 00:32:40 +0900

    レイアウト等の修正

--- app/store/posts.js
+++ app/store/posts.js
@@ -42,7 +42,7 @@ export const actions = {
   async publishPost({ commit }, { payload }) {
     const user = await this.$axios.$get(`/users/${payload.user.id}.json`)
     const post_id = (await this.$axios.$post('/posts.json', payload)).name
-    const created_at = moment().format()
+    const created_at = moment().format('YYYY-MM-DD HH:mm:ss')
     const post = { id: post_id, ...payload, created_at }
     const putData = { id: post_id, ...payload, created_at }
     delete putData.user
